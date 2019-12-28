74450b15595e746b4a444e96f4a7d9daa87bd528
AamnaKhursheed <55755634+AamnaKhursheed@users.noreply.github.com>
2019-12-28 23:06:18 +0500

    Add files via upload

---
+++ Sets.py
@@ -0,0 +1,16 @@
+
+
+S = {0, 2, 4, 6, 8};
+A = {1, 2, 3, 4, 5};
+
+# symmetric difference
+print("Symmetric difference of E and N is",S ^ A)
+
+# union
+print("Union of E and N is",S | A)
+
+# intersection
+print("Intersection of E and N is",S & A)
+
+# difference
+print("Difference of E and N is",S - A)
